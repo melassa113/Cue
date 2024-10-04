fdf08f58373218156e1bf6f29ce76ab1723f2bb5
melassa113 <shkolnyyiai@gmail.com>
2024-10-04 15:46:42 +0700

    bored

---
+++ CIE.h
@@ -0,0 +1,78 @@
+#ifndef RUS_H_INCLUDED
+#define RUS_H_INCLUDED
+#include <stdio.h>
+
+
+#define неведомо srand(time(NULL));
+
+#define поболе >=
+#define помене <=
+#define боле >
+#define мене <
+#define аки ==
+#define стане =
+#define не !=
+#define да +
+#define величать ++
+#define без -
+#define на *
+#define дели /
+#define ъ ;
+#define сталось %
+#define право true
+#define бреше false
+
+#define куча [ 
+#define мала ]
+
+#define долг *(-1)
+#define дулю 0
+#define кол 1
+#define пара 2 
+#define пала 5
+#define дюжа 12
+#define азъбуки 'А'
+
+#define коли if(
+#define пущай ){
+#define ежели }else if(
+#define либо }else{
+#define аминь }
+
+#define покуда while(
+#define твори do{
+#define доколе }while(
+#define буде );
+#define откель for(
+#define ступай goto
+#define вон exit
+
+#define твердь const
+#define цело int
+#define дробно float
+#define передробно double
+#define литра char
+#define непотребо void
+
+#define кличут &
+#define молви printf(
+#define литера "%c"
+#define цифирь "%d"
+#define який ,
+#define поду printf("\n");
+#define поведай scanf_s(
+#define познай cin>>
+
+#define молитва int
+#define возглас void
+#define отченаш main(){
+#define воздати return
+#define чтеть ()
+#define писати (){
+
+#define пути_господни_неисповедимы srand(time(NULL));
+#define воля_божья rand()
+#define жди system("pause");
+#define по_русски setlocale(LC_ALL, "Russian");
+
+#endif // RUS_H_INCLUDED
